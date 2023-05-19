b9dec3980c65b34afc4d4aa3d079c983ff9398cf
karyanayandi <halo@yandi.me>
2023-05-19 13:40:04 +0700

    feat(web): add global error and not found page

---
+++ packages/web/src/app/not-found.tsx
@@ -0,0 +1,26 @@
+import NextLink from "next/link"
+import { Button } from "ui"
+
+export default function NotFound() {
+  return (
+    <section className="bg-background flex h-screen items-center justify-center">
+      <div className="mx-auto max-w-screen-xl px-4 py-8 lg:px-6 lg:py-16">
+        <div className="mx-auto max-w-screen-sm text-center">
+          <h1 className="text-primary mb-4 text-7xl font-extrabold tracking-tight lg:text-9xl">
+            404
+          </h1>
+          <p className="text-foreground mb-4 text-3xl font-bold tracking-tight md:text-4xl">
+            Something&apos;s missing.
+          </p>
+          <p className="text-foreground mb-4 text-lg font-light">
+            Sorry, we can&apos;t find that page. You&apos;ll find lots to
+            explore on the home page.
+          </p>
+          <NextLink aria-label="Go To Homepage" href="/">
+            <Button aria-label="Go To Homepage">Back to Homepage</Button>
+          </NextLink>
+        </div>
+      </div>
+    </section>
+  )
+}
