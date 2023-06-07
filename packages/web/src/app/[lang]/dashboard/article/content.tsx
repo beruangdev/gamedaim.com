f5f832fe1671cf22640643e0b894f0f5ca7f2a20
ghizzkira <tiadiagis@gmail.com>
2023-06-07 14:35:57 +0700

    fix(web): fix colors

--- packages/web/src/app/[lang]/dashboard/article/content.tsx
+++ packages/web/src/app/[lang]/dashboard/article/content.tsx
@@ -23,33 +23,40 @@ const ActionDashboard = dynamic(() =>
   import("@/components/Action").then((mod) => mod.ActionDashboard),
 )
 export function ArticleDashboardContent() {
-  const { articlesCount: articlesCountId } = useGetArticlesCountByLang("id_ID")
-  const { articlesCount: articlesCountEn } = useGetArticlesCountByLang("en_US")
-  const lastPageId = articlesCountId && Math.ceil(articlesCountId / 10)
-  const lastPageEn = articlesCountEn && Math.ceil(articlesCountEn / 10)
   const [isLoading, setIsLoading] = React.useState<boolean>(true)
   const [searchQuery, setSearchQuery] = React.useState<string>("")
   const [searchQueryEn, setSearchQueryEn] = React.useState<string>("")
   const [searchType, setSearchType] = React.useState("id_ID")
-  const [pageId, setPageId] = React.useState<number>(1)
-  const [pageEn, setPageEn] = React.useState<number>(1)
-  const [articlesDataId, setArticlesDataId] = React.useState<
+  const [pageLangId, setPageLangId] = React.useState<number>(1)
+  const [pageLangEn, setPageLangEn] = React.useState<number>(1)
+  const [articlesDataLangId, setArticlesDataLangId] = React.useState<
     ArticleDataProps[]
   >([])
-  const [articlesDataEn, setArticlesDataEn] = React.useState<
+  const [articlesDataLangEn, setArticlesDataLangEn] = React.useState<
     ArticleDataProps[]
   >([])
-  const { articles, updatedArticles } = useGetArticles("id_ID", pageId)
+
+  // Hooks get Articles by Lang
+
+  const { articlesCount: articlesCountLangId } =
+    useGetArticlesCountByLang("id_ID")
+  const { articlesCount: articlesCounLangtEn } =
+    useGetArticlesCountByLang("en_US")
+  const lastPageLangId =
+    articlesCountLangId && Math.ceil(articlesCountLangId / 10)
+  const lastPageLangEn =
+    articlesCounLangtEn && Math.ceil(articlesCounLangtEn / 10)
+  const { articles, updatedArticles } = useGetArticles("id_ID", pageLangId)
   const {
-    articles: resultArticlesId,
-    updatedArticles: updatedResultArticlesId,
+    articles: resultArticlesLangId,
+    updatedArticles: updatedResultArticlesLangId,
   } = useSearchDashboardArticles("id_ID", searchQuery)
 
-  const { articles: articlesEn, updatedArticles: updatedArticlesEn } =
-    useGetArticles("en_US", pageEn)
+  const { articles: articlesLangEn, updatedArticles: updatedArticlesLangEn } =
+    useGetArticles("en_US", pageLangEn)
   const {
-    articles: resultArticlesEn,
-    updatedArticles: updatedResultArticlesEn,
+    articles: resultArticlesLangEn,
+    updatedArticles: updatedResultArticlesLangEn,
   } = useSearchDashboardArticles("en_US", searchQuery)
 
   const handleSearchOnChange = (e: React.ChangeEvent<HTMLInputElement>) => {
@@ -63,20 +70,20 @@ export function ArticleDashboardContent() {
 
   React.useEffect(() => {
     if (searchQuery) {
-      setArticlesDataId(resultArticlesId)
+      setArticlesDataLangId(resultArticlesLangId)
     } else {
-      setArticlesDataId(articles)
+      setArticlesDataLangId(articles)
     }
     if (searchQueryEn) {
-      setArticlesDataEn(resultArticlesEn)
+      setArticlesDataLangEn(resultArticlesLangEn)
     } else {
-      setArticlesDataEn(articlesEn)
+      setArticlesDataLangEn(articlesLangEn)
     }
   }, [
     articles,
-    articlesEn,
-    resultArticlesEn,
-    resultArticlesId,
+    articlesLangEn,
+    resultArticlesLangEn,
+    resultArticlesLangId,
     searchQuery,
     searchQueryEn,
   ])
@@ -119,17 +126,20 @@ export function ArticleDashboardContent() {
               </TabsTrigger>
             </TabsList>
             <TabsContent value="id_ID">
-              {articlesDataId !== undefined && articlesDataId.length > 0 ? (
+              {articlesDataLangId !== undefined &&
+              articlesDataLangId.length > 0 ? (
                 <TableArticle
-                  articles={articlesDataId}
+                  articles={articlesDataLangId}
                   searchQuery={searchQuery}
-                  updateResultArticles={updatedResultArticlesId}
+                  updateResultArticles={updatedResultArticlesLangId}
                   updateArticles={updatedArticles}
-                  page={pageId}
-                  lastPage={lastPageId}
-                  handleBack={() => setPageId((old) => Math.max(old - 1, 0))}
+                  page={pageLangId}
+                  lastPage={lastPageLangId}
+                  handleBack={() =>
+                    setPageLangId((old) => Math.max(old - 1, 0))
+                  }
                   handleNext={() => {
-                    setPageId((old) => old + 1)
+                    setPageLangId((old) => old + 1)
                   }}
                 />
               ) : (
@@ -141,17 +151,20 @@ export function ArticleDashboardContent() {
               )}
             </TabsContent>
             <TabsContent value="en_US">
-              {articlesDataEn !== undefined && articlesDataEn.length > 0 ? (
+              {articlesDataLangEn !== undefined &&
+              articlesDataLangEn.length > 0 ? (
                 <TableArticle
-                  articles={articlesDataEn}
+                  articles={articlesDataLangEn}
                   searchQuery={searchQueryEn}
-                  updateResultArticles={updatedResultArticlesEn}
-                  updateArticles={updatedArticlesEn}
-                  page={pageEn}
-                  lastPage={lastPageEn}
-                  handleBack={() => setPageEn((old) => Math.max(old - 1, 0))}
+                  updateResultArticles={updatedResultArticlesLangEn}
+                  updateArticles={updatedArticlesLangEn}
+                  page={pageLangEn}
+                  lastPage={lastPageLangEn}
+                  handleBack={() =>
+                    setPageLangEn((old) => Math.max(old - 1, 0))
+                  }
                   handleNext={() => {
-                    setPageEn((old) => old + 1)
+                    setPageLangEn((old) => old + 1)
                   }}
                 />
               ) : (
