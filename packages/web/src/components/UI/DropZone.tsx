631a5be099094f7b81bca809556fd856b3700e1b
beruangdev <beruangdev@gmail.com>
2023-06-08 13:19:42 +0700

    feat: DropZone.tsx add drag and drop feat

--- packages/web/src/components/UI/DropZone.tsx
+++ packages/web/src/components/UI/DropZone.tsx
@@ -16,6 +16,37 @@ export const DropZone = React.forwardRef<HTMLDivElement, DropZoneProps>(
       ...rest
     } = props
 
+    const [isDragging, setIsDragging] = React.useState(false)
+
+    const handleDragEnter = (event: React.DragEvent<HTMLLabelElement>) => {
+      event.preventDefault()
+      setIsDragging(true)
+    }
+
+    const handleDragLeave = () => {
+      setIsDragging(false)
+    }
+
+    const handleDragOver = (event: React.DragEvent<HTMLLabelElement>) => {
+      event.preventDefault()
+    }
+
+    const handleDrop = (event: React.DragEvent<HTMLLabelElement>) => {
+      event.preventDefault()
+      setIsDragging(false)
+
+      const files = event.dataTransfer.files
+
+      const inputElement = document.getElementById(
+        "dropzone-file",
+      ) as HTMLInputElement
+      if (inputElement) {
+        inputElement.files = files
+      }
+
+      event.dataTransfer.clearData()
+    }
+
     return (
       <div
         ref={ref}
@@ -23,7 +54,13 @@ export const DropZone = React.forwardRef<HTMLDivElement, DropZoneProps>(
       >
         <label
           htmlFor="dropzone-file"
-          className="border-border/30 bg-background/5 hover:bg-background/10 flex h-48 w-full cursor-pointer flex-col items-center justify-center rounded-lg border-2 border-dashed"
+          className={`border-border/30 bg-background/5 hover:bg-background/10 flex h-48 w-full cursor-pointer flex-col items-center justify-center rounded-lg border-2 border-dashed ${
+            isDragging ? "border-primary" : ""
+          }`}
+          onDragEnter={handleDragEnter}
+          onDragLeave={handleDragLeave}
+          onDragOver={handleDragOver}
+          onDrop={handleDrop}
         >
           <div className="flex flex-col items-center justify-center pb-6 pt-5">
             <svg
