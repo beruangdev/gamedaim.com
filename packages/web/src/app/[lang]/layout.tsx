543b343a21bd80a4e69a293a17e6d75d45908429
karyanayandi <halo@yandi.me>
2023-06-09 14:28:46 +0700

    feat(web): generate meta data index page

--- packages/web/src/app/[lang]/layout.tsx
+++ packages/web/src/app/[lang]/layout.tsx
@@ -6,48 +6,68 @@ import env from "env"
 import { Toaster } from "@/components/UI/Toast"
 import { ThemeProvider } from "@/components/Theme"
 import "./globals.css"
+import { getSettingByKeyAction } from "@/lib/api/server/setting"
 
 const inter = Inter({ subsets: ["latin"] })
 
-export const metadata: Metadata = {
-  title: {
-    default: env.SITE_TITLE,
-    template: `%s | ${env.SITE_TITLE}`,
-  },
-  description: env.ABOUT,
-  openGraph: {
-    title: env.SITE_TITLE,
-    description: env.ABOUT,
-    url: `https://${env.DOMAIN}`,
-    siteName: env.SITE_TITLE,
-    images: [
-      {
-        url: env.LOGO_OG_URL,
-        width: env.LOGO_OG_WIDTH,
-        height: env.LOGO_OG_HEIGHT,
+export async function generateMetadata(): Promise<Metadata> {
+  const { data: siteTitle } = await getSettingByKeyAction("siteTitle")
+  const { data: siteTagline } = await getSettingByKeyAction("siteTagline")
+  const { data: siteDescription } = await getSettingByKeyAction(
+    "siteDescription",
+  )
+  const { data: siteDomain } = await getSettingByKeyAction("siteDomain")
+  const { data: twitterUsername } = await getSettingByKeyAction(
+    "twitterUsername",
+  )
+
+  return {
+    title: {
+      default: `${siteTitle?.value} | ${siteTagline?.value}` || env.SITE_TITLE,
+      template: `%s | ${siteTitle?.value || env.SITE_TITLE}`,
+    },
+    description: siteDescription?.value || env.ABOUT,
+    alternates: {
+      canonical: `https:/${siteDomain?.value}/`,
+      languages: {
+        "id-ID": "/",
+        "en-US": `https://global.${siteDomain?.value}`,
       },
-    ],
-    locale: "id-ID",
-    type: "website",
-  },
-  robots: {
-    index: true,
-    follow: true,
-    googleBot: {
+    },
+    openGraph: {
+      title: siteTitle?.value || env.SITE_TITLE,
+      description: siteDescription?.value || env.ABOUT,
+      url: `https://${siteDomain?.value || env.DOMAIN}`,
+      siteName: siteTitle?.value || env.SITE_TITLE,
+      images: [
+        {
+          url: env.LOGO_OG_URL,
+          width: env.LOGO_OG_WIDTH,
+          height: env.LOGO_OG_HEIGHT,
+        },
+      ],
+      locale: "id-ID",
+      type: "website",
+    },
+    robots: {
       index: true,
       follow: true,
-      "max-video-preview": -1,
-      "max-image-preview": "large",
-      "max-snippet": -1,
+      googleBot: {
+        index: true,
+        follow: true,
+        "max-video-preview": -1,
+        "max-image-preview": "large",
+        "max-snippet": -1,
+      },
     },
-  },
-  twitter: {
-    title: env.TWITTER_USERNAME,
-    card: "summary_large_image",
-  },
-  icons: {
-    shortcut: "/favicon.ico",
-  },
+    twitter: {
+      title: twitterUsername?.value || env.TWITTER_USERNAME,
+      card: "summary_large_image",
+    },
+    icons: {
+      shortcut: "/favicon.ico",
+    },
+  }
 }
 
 interface RootLayoutProps {
