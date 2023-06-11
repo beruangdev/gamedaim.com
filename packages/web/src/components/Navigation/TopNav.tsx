0ea93a6915af00eaf5e8d1460f240e1743006fde
ghizzkira <tiadiagis@gmail.com>
2023-06-11 16:24:43 +0700

    fix(web): profil page

--- packages/web/src/components/Navigation/TopNav.tsx
+++ packages/web/src/components/Navigation/TopNav.tsx
@@ -39,7 +39,7 @@ export const TopNav = React.forwardRef<HTMLDivElement, TopNavProps>(
 
     return (
       <header
-        className="opacity-1 dark:bg-theme-900 fixed left-auto top-0 z-[49] -my-0 mx-auto box-border flex h-16 w-full items-center border-none bg-white px-2 py-0 align-baseline shadow-lg outline-none"
+        className="opacity-1 bg-background fixed left-auto top-0 z-[49] -my-0 mx-auto box-border flex h-16 w-full items-center border-none px-2 py-0 align-baseline shadow-lg outline-none"
         {...rest}
       >
         <div className="relative ml-auto mr-auto grow px-3">
